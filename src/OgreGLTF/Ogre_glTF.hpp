61405cbbac9100047c64bfa35307743633c26264
Matt Chang <chchwy@gmail.com>
2019-02-18 10:23:57 +1100

    Format documents

--- src/OgreGLTF/Ogre_glTF.hpp
+++ src/OgreGLTF/Ogre_glTF.hpp
@@ -14,195 +14,195 @@ class Node;
 namespace Ogre_glTF
 {
 
-	//Forward declare main class
-	class glTFLoader;
+//Forward declare main class
+class glTFLoader;
 
-	///Structure that contains the information of the model that you loaded
-	struct ModelInformation
-	{
-		///Easy method to build an item to use
-		/// \param smgr pointer to the scene manager where to create the item
-		/// \param sceneType tell if it will be static or dynamic
-		Ogre::Item* makeItem(Ogre::SceneManager* smgr, Ogre::SceneMemoryMgrTypes sceneType = Ogre::SCENE_DYNAMIC)
-		{
-			auto item = smgr->createItem(mesh, sceneType);
-			for(size_t i = 0; i < item->getNumSubItems(); ++i) { item->getSubItem(i)->setDatablock(pbrMaterialList[i]); }
-			return item;
-		}
+///Structure that contains the information of the model that you loaded
+struct ModelInformation
+{
+    ///Easy method to build an item to use
+    /// \param smgr pointer to the scene manager where to create the item
+    /// \param sceneType tell if it will be static or dynamic
+    Ogre::Item* makeItem(Ogre::SceneManager* smgr, Ogre::SceneMemoryMgrTypes sceneType = Ogre::SCENE_DYNAMIC)
+    {
+        auto item = smgr->createItem(mesh, sceneType);
+        for (size_t i = 0; i < item->getNumSubItems(); ++i) { item->getSubItem(i)->setDatablock(pbrMaterialList[i]); }
+        return item;
+    }
 
-		///Smart pointer to the loaded mesh
-		Ogre::MeshPtr mesh;
-		///List of materials that correspond to each of the submeshes in the model
-		std::vector<Ogre::HlmsDatablock*> pbrMaterialList;
+    ///Smart pointer to the loaded mesh
+    Ogre::MeshPtr mesh;
+    ///List of materials that correspond to each of the submeshes in the model
+    std::vector<Ogre::HlmsDatablock*> pbrMaterialList;
 
-		///Local transform on the glTF node this model came from
-		struct ModelTransform
-		{
-			Ogre::Vector3 position		 = Ogre::Vector3::ZERO;
-			Ogre::Vector3 scale			 = Ogre::Vector3::UNIT_SCALE;
-			Ogre::Quaternion orientation = Ogre::Quaternion::IDENTITY;
+    ///Local transform on the glTF node this model came from
+    struct ModelTransform
+    {
+        Ogre::Vector3 position = Ogre::Vector3::ZERO;
+        Ogre::Vector3 scale = Ogre::Vector3::UNIT_SCALE;
+        Ogre::Quaternion orientation = Ogre::Quaternion::IDENTITY;
 
-			void apply (Ogre::SceneNode* node)
-			{
-				node->setPosition(position);
-				node->setScale(scale);
-				node->setOrientation(orientation);
-			}
-		} transform;
-	};
+        void apply(Ogre::SceneNode* node)
+        {
+            node->setPosition(position);
+            node->setScale(scale);
+            node->setOrientation(orientation);
+        }
+    } transform;
+};
 
-	///Plugin accessible interface that plugin users can use
-	struct glTFLoaderInterface
-	{
-		///Polymorphic dtor
-		virtual ~glTFLoaderInterface() = default;
+///Plugin accessible interface that plugin users can use
+struct glTFLoaderInterface
+{
+    ///Polymorphic dtor
+    virtual ~glTFLoaderInterface() = default;
 
-		///location where to load the data
-		enum class LoadFrom { FileSystem, ResourceManager };
+    ///location where to load the data
+    enum class LoadFrom { FileSystem, ResourceManager };
 
-		///Get the model data
-		///\param modelName name of the resource being loaded
-		///\param loadLocation flag that signal if the model is loaded directly from the filesystem, or from Ogre's resource manager
-		virtual ModelInformation getModelData(const std::string& modelName, LoadFrom loadLocation) = 0;
+    ///Get the model data
+    ///\param modelName name of the resource being loaded
+    ///\param loadLocation flag that signal if the model is loaded directly from the filesystem, or from Ogre's resource manager
+    virtual ModelInformation getModelData(const std::string& modelName, LoadFrom loadLocation) = 0;
 
-        virtual Ogre::SceneNode* createScene(const std::string& modelName, LoadFrom loadLocation,
-                                 Ogre::SceneManager* smgr, Ogre::SceneNode* parentNode = nullptr) = 0;
-	};
+    virtual Ogre::SceneNode* createScene(const std::string& modelName, LoadFrom loadLocation,
+                                         Ogre::SceneManager* smgr, Ogre::SceneNode* parentNode = nullptr) = 0;
+};
 
-	///Class that hold the loaded content of a glTF file and that can create Ogre objects from it
-	class loaderAdapter
-	{
-		friend class glTFLoader;
+///Class that hold the loaded content of a glTF file and that can create Ogre objects from it
+class loaderAdapter
+{
+    friend class glTFLoader;
 
-		///opaque content of the class
-		struct impl;
+    ///opaque content of the class
+    struct impl;
 
-		///pointer to implementation
-		std::unique_ptr<impl> pimpl;
+    ///pointer to implementation
+    std::unique_ptr<impl> pimpl;
 
-		std::string adapterName;
+    std::string adapterName;
 
-	public:
-		///This will also initialize the "pimpl" structure
-		loaderAdapter();
+public:
+    ///This will also initialize the "pimpl" structure
+    loaderAdapter();
 
-		///This clear the pimpl structure
-		~loaderAdapter();
+    ///This clear the pimpl structure
+    ~loaderAdapter();
 
-		///Deleted copy constructor : non copyable class
-		loaderAdapter(const loaderAdapter&) = delete;
+    ///Deleted copy constructor : non copyable class
+    loaderAdapter(const loaderAdapter&) = delete;
 
-		///Deleted assignment constructor : non copyable class
-		loaderAdapter& operator=(const loaderAdapter&) = delete;
+    ///Deleted assignment constructor : non copyable class
+    loaderAdapter& operator=(const loaderAdapter&) = delete;
 
-		///Return the mesh loaded from the glTF file
-		Ogre::MeshPtr getMesh() const;
+    ///Return the mesh loaded from the glTF file
+    Ogre::MeshPtr getMesh() const;
 
-        std::vector<Ogre::MeshPtr> getAllMeshes() const;
+    std::vector<Ogre::MeshPtr> getAllMeshes() const;
 
-		///Return one of the datablocks loaded from the gltf file.
-		///\param index of the datablocks used. In a multi-material file, it should be one different per primitive (submesh).
-		Ogre::HlmsDatablock* getDatablock(size_t index = 0) const;
+    ///Return one of the datablocks loaded from the gltf file.
+    ///\param index of the datablocks used. In a multi-material file, it should be one different per primitive (submesh).
+    Ogre::HlmsDatablock* getDatablock(size_t index = 0) const;
 
-		///Return the number of datablock stored
-		size_t getDatablockCount();
+    ///Return the number of datablock stored
+    size_t getDatablockCount();
 
-		///Get the local transform to apply to the node to align the model with what you would expect.
-		ModelInformation::ModelTransform getTransform();
+    ///Get the local transform to apply to the node to align the model with what you would expect.
+    ModelInformation::ModelTransform getTransform();
 
-		///Construct an item for this object
-		/// \param smgr pointer to the scene manager where we are creating the item
-		Ogre::Item* getItem(Ogre::SceneManager* smgr) const;
+    ///Construct an item for this object
+    /// \param smgr pointer to the scene manager where we are creating the item
+    Ogre::Item* getItem(Ogre::SceneManager* smgr) const;
 
-		///Move constructor : object is movable
-		/// \param other object to move
-		loaderAdapter(loaderAdapter&& other) noexcept;
+    ///Move constructor : object is movable
+    /// \param other object to move
+    loaderAdapter(loaderAdapter&& other) noexcept;
 
-		///Move assignment operator
-		loaderAdapter& operator=(loaderAdapter&& other) noexcept;
+    ///Move assignment operator
+    loaderAdapter& operator=(loaderAdapter&& other) noexcept;
 
-		///Return the current state of the adapter
-		bool isOk() const;
+    ///Return the current state of the adapter
+    bool isOk() const;
 
-		///Return the last error generated by the underlying glTF loading library
-		std::string getLastError() const;
-        Ogre::SceneNode* makeScene(Ogre::SceneManager* smgr, Ogre::SceneNode* parentNode);
-        void makeSceneNode(Ogre::SceneNode* parentNode);
-        void makeNode(Ogre::SceneNode* ogreNode, const tinygltf::Node& node, Ogre::SceneManager* smgr, const std::vector<Ogre::MeshPtr>& meshes);
-    };
+    ///Return the last error generated by the underlying glTF loading library
+    std::string getLastError() const;
+    Ogre::SceneNode* makeScene(Ogre::SceneManager* smgr, Ogre::SceneNode* parentNode);
+    void makeSceneNode(Ogre::SceneNode* parentNode);
+    void makeNode(Ogre::SceneNode* ogreNode, const tinygltf::Node& node, Ogre::SceneManager* smgr, const std::vector<Ogre::MeshPtr>& meshes);
+};
 
-	///Class that is responsible for initializing the library with the loader, and giving out
-	class glTFLoader final : public glTFLoaderInterface
-	{
-		///object that acutally communicate with the underlying glTF loading library
-		struct glTFLoaderImpl;
+///Class that is responsible for initializing the library with the loader, and giving out
+class glTFLoader final : public glTFLoaderInterface
+{
+    ///object that acutally communicate with the underlying glTF loading library
+    struct glTFLoaderImpl;
 
-		///Opaque pointer that handle the underlying glTF loading library (pimpl)
-		std::unique_ptr<glTFLoaderImpl> loaderImpl;
+    ///Opaque pointer that handle the underlying glTF loading library (pimpl)
+    std::unique_ptr<glTFLoaderImpl> loaderImpl;
 
-	public:
-		///Initialize the library by creating this object.
-		glTFLoader();
+public:
+    ///Initialize the library by creating this object.
+    glTFLoader();
 
-		///Move constructor
-		/// \param other object to move
-		glTFLoader(glTFLoader&& other) noexcept;
+    ///Move constructor
+    /// \param other object to move
+    glTFLoader(glTFLoader&& other) noexcept;
 
-		///Move assign operator
-		glTFLoader& operator=(glTFLoader&& other) noexcept;
+    ///Move assign operator
+    glTFLoader& operator=(glTFLoader&& other) noexcept;
 
-		///Deinitialize the library at this object destruction
-		~glTFLoader();
+    ///Deinitialize the library at this object destruction
+    ~glTFLoader();
 
-		///Load a glTF text or binary file. Give you an adapter to use this file with Ogre
-		/// \param path String containing the path to a file to load (either .glTF or .glc)
-		loaderAdapter loadFromFileSystem(const std::string& path) const;
+    ///Load a glTF text or binary file. Give you an adapter to use this file with Ogre
+    /// \param path String containing the path to a file to load (either .glTF or .glc)
+    loaderAdapter loadFromFileSystem(const std::string& path) const;
 
-		///Load a GLB from Ogre's resource manager
-		loaderAdapter loadGlbResource(const std::string& name) const;
+    ///Load a GLB from Ogre's resource manager
+    loaderAdapter loadGlbResource(const std::string& name) const;
 
-		///Get the model data. Contains everything you need to create object from the model contained on the glTF asset
-		ModelInformation getModelData(const std::string& modelName, LoadFrom loadLocation) override;
+    ///Get the model data. Contains everything you need to create object from the model contained on the glTF asset
+    ModelInformation getModelData(const std::string& modelName, LoadFrom loadLocation) override;
 
-        Ogre::SceneNode* createScene(const std::string& modelName, LoadFrom loadLocation,
-                         Ogre::SceneManager* smgr, Ogre::SceneNode* parentNode = nullptr) override;
+    Ogre::SceneNode* createScene(const std::string& modelName, LoadFrom loadLocation,
+                                 Ogre::SceneManager* smgr, Ogre::SceneNode* parentNode = nullptr) override;
 
-		///Deleted copy constructor
-		glTFLoader(const glTFLoader&) = delete;
+    ///Deleted copy constructor
+    glTFLoader(const glTFLoader&) = delete;
 
-		///Deleted assignment operator
-		glTFLoader& operator=(const glTFLoader&) = delete;
-	};
+    ///Deleted assignment operator
+    glTFLoader& operator=(const glTFLoader&) = delete;
+};
 
-	class Exception : public Ogre::Exception
-	{
-	public:
-		Exception(int number, const std::string& desc, const std::string& src) : Ogre::Exception(number, "gltf exception: " + desc, "gltf: " + src) {}
-	};
+class Exception : public Ogre::Exception
+{
+public:
+    Exception(int number, const std::string& desc, const std::string& src) : Ogre::Exception(number, "gltf exception: " + desc, "gltf: " + src) {}
+};
 
-	class FileIOError : public Exception
-	{
-	public:
-		FileIOError(const std::string& message = "", const std::string& src = "") : Exception(ERR_FILE_NOT_FOUND, "couldn't open file: " + message, src) {}
-	};
+class FileIOError : public Exception
+{
+public:
+    FileIOError(const std::string& message = "", const std::string& src = "") : Exception(ERR_FILE_NOT_FOUND, "couldn't open file: " + message, src) {}
+};
 
-	class RootNotInitializedYet : public Exception
-	{
-	public:
-		RootNotInitializedYet(const std::string& message = "", const std::string& src = "") : Exception(ERR_INVALID_CALL, message, src) {}
-	};
+class RootNotInitializedYet : public Exception
+{
+public:
+    RootNotInitializedYet(const std::string& message = "", const std::string& src = "") : Exception(ERR_INVALID_CALL, message, src) {}
+};
 
-	class LoadingError : public Exception
-	{
-	public:
-		LoadingError(const std::string& message, const std::string& src = "") : Exception(ERR_INTERNAL_ERROR, "while loading: " + message, src) {}
-	};
+class LoadingError : public Exception
+{
+public:
+    LoadingError(const std::string& message, const std::string& src = "") : Exception(ERR_INTERNAL_ERROR, "while loading: " + message, src) {}
+};
 
-	class InitError : public Exception
-	{
-	public:
-		InitError(const std::string& message, const std::string& src = "") : Exception(ERR_INVALID_STATE, message, src) {}
-	};
+class InitError : public Exception
+{
+public:
+    InitError(const std::string& message, const std::string& src = "") : Exception(ERR_INVALID_STATE, message, src) {}
+};
 }
 
 //To facilitate the use of the library:
--- src/OgreGLTF/Ogre_glTF_skeletonImporter.hpp
+++ src/OgreGLTF/Ogre_glTF_skeletonImporter.hpp
@@ -7,82 +7,82 @@
 namespace Ogre_glTF
 {
 
-	class skeletonImporter
-	{
-		///Reference to the model
-		tinygltf::Model& model;
+class skeletonImporter
+{
+    ///Reference to the model
+    tinygltf::Model& model;
 
-		using tinygltfJointNodeIndex = int;
+    using tinygltfJointNodeIndex = int;
 
-		///number to increment when creating strings for skeleton with no names in glTF files
-		static int skeletonID;
+    ///number to increment when creating strings for skeleton with no names in glTF files
+    static int skeletonID;
 
-		///Pointer to the skeleton object we are currently working on.
-		Ogre::v1::SkeletonPtr skeleton;
+    ///Pointer to the skeleton object we are currently working on.
+    Ogre::v1::SkeletonPtr skeleton;
 
-		///Recurisve fucntion : Create a bone for each children, and each children's children...
-		/// \param skinName name of the skin
-		/// \param childs array contaning the indices of the childrens
-		/// \param parent a pointer to a bone that is part of the skeleton we are creating
-		void addChidren(const std::string& skinName, const std::vector<int>& childs, Ogre::v1::OldBone* parent, const std::vector<int>& joints);
+    ///Recurisve fucntion : Create a bone for each children, and each children's children...
+    /// \param skinName name of the skin
+    /// \param childs array contaning the indices of the childrens
+    /// \param parent a pointer to a bone that is part of the skeleton we are creating
+    void addChidren(const std::string& skinName, const std::vector<int>& childs, Ogre::v1::OldBone* parent, const std::vector<int>& joints);
 
-		///Get the "skeleton node" from the skin object, call add children on it
-		/// \param skin tinigltf skin object we are loading
-		/// \param rootBone a freshly created bone object from an Ogre::SkeletonPtr
-		/// \param name Name of the skeleton we are loading
-		void loadBoneHierarchy(const tinygltf::Skin& skin, Ogre::v1::OldBone* rootBone, const std::string& name);
+    ///Get the "skeleton node" from the skin object, call add children on it
+    /// \param skin tinigltf skin object we are loading
+    /// \param rootBone a freshly created bone object from an Ogre::SkeletonPtr
+    /// \param name Name of the skeleton we are loading
+    void loadBoneHierarchy(const tinygltf::Skin& skin, Ogre::v1::OldBone* rootBone, const std::string& name);
 
-		///Represent a keyframe as laded from tinygltf, but converted to Ogre objects
-		struct keyFrame
-		{
-			float timePoint{ -1 };
-			float weights{};
-			Ogre::Quaternion rotation{};
-			Ogre::Vector3 position, scale{};
-		};
+    ///Represent a keyframe as laded from tinygltf, but converted to Ogre objects
+    struct keyFrame
+    {
+        float timePoint{ -1 };
+        float weights{};
+        Ogre::Quaternion rotation{};
+        Ogre::Vector3 position, scale{};
+    };
 
-		///Vector of keyframes
-		using keyFrameList = std::vector<keyFrame>;
+    ///Vector of keyframes
+    using keyFrameList = std::vector<keyFrame>;
 
-		///Type for a list of animation channels
-		using channelList = std::vector<std::reference_wrapper<tinygltf::AnimationChannel>>;
+    ///Type for a list of animation channels
+    using channelList = std::vector<std::reference_wrapper<tinygltf::AnimationChannel>>;
 
-		///Type for holding the mapping beween Bone index and glTF nodes
-		using nodeIndexConversionMap = std::unordered_map<tinygltfJointNodeIndex, tinygltfJointNodeIndex>;
+    ///Type for holding the mapping beween Bone index and glTF nodes
+    using nodeIndexConversionMap = std::unordered_map<tinygltfJointNodeIndex, tinygltfJointNodeIndex>;
 
-		///Node -> joint conversion map
-		nodeIndexConversionMap nodeToJointMap;
+    ///Node -> joint conversion map
+    nodeIndexConversionMap nodeToJointMap;
 
-		///Hold the list of the bind matrices. These are the inverse of the inverse bind matrices of the skin. Represent transforms that put each bone's into it's binding pose
-		std::vector<Ogre::Matrix4> bindMatrices;
+    ///Hold the list of the bind matrices. These are the inverse of the inverse bind matrices of the skin. Represent transforms that put each bone's into it's binding pose
+    std::vector<Ogre::Matrix4> bindMatrices;
 
-		///Load from an animation channel sampler the time data, and ore it inside the keyframe
-		void loadTimepointFromSamplerToKeyFrame(int bone, int frameID, int& count, keyFrame& animationFrame, tinygltf::AnimationSampler& sampler);
+    ///Load from an animation channel sampler the time data, and ore it inside the keyframe
+    void loadTimepointFromSamplerToKeyFrame(int bone, int frameID, int& count, keyFrame& animationFrame, tinygltf::AnimationSampler& sampler);
 
-		///Load vector of 3 doubles or 3 floats from the animation sampler, and store them into a vector
-		void loadVector3FromSampler(int frameID, int& count, tinygltf::AnimationSampler& sampler, Ogre::Vector3& vector);
+    ///Load vector of 3 doubles or 3 floats from the animation sampler, and store them into a vector
+    void loadVector3FromSampler(int frameID, int& count, tinygltf::AnimationSampler& sampler, Ogre::Vector3& vector);
 
-		///Load vector of 4 doubles or 4 floats from the animation sampler, and store them into a quaternion
-		void loadQuatFromSampler(int frameID, int& count, tinygltf::AnimationSampler& sampler, Ogre::Quaternion& quat) const;
+    ///Load vector of 4 doubles or 4 floats from the animation sampler, and store them into a quaternion
+    void loadQuatFromSampler(int frameID, int& count, tinygltf::AnimationSampler& sampler, Ogre::Quaternion& quat) const;
 
-		///Goes throught the list of animation channel, and assign the 4 given pointers to the one that correspond to it
-		void detectAnimationChannel(const channelList& channels, tinygltf::AnimationChannel*& translation, tinygltf::AnimationChannel*& rotation, tinygltf::AnimationChannel*& scale, tinygltf::AnimationChannel*& weights) const;
+    ///Goes throught the list of animation channel, and assign the 4 given pointers to the one that correspond to it
+    void detectAnimationChannel(const channelList& channels, tinygltf::AnimationChannel*& translation, tinygltf::AnimationChannel*& rotation, tinygltf::AnimationChannel*& scale, tinygltf::AnimationChannel*& weights) const;
 
-		///Load keyframe data from the samplers of each AnimationChannel into a KeyFrame object
-		void loadKeyFrameDataFromSampler(const tinygltf::Animation& animation, int bone, tinygltf::AnimationChannel* translation, tinygltf::AnimationChannel* rotation, tinygltf::AnimationChannel* scale, tinygltf::AnimationChannel* weights, int frameID, int& count, keyFrame& animationFrame);
+    ///Load keyframe data from the samplers of each AnimationChannel into a KeyFrame object
+    void loadKeyFrameDataFromSampler(const tinygltf::Animation& animation, int bone, tinygltf::AnimationChannel* translation, tinygltf::AnimationChannel* rotation, tinygltf::AnimationChannel* scale, tinygltf::AnimationChannel* weights, int frameID, int& count, keyFrame& animationFrame);
 
-		///Load all keyframes for the animation
-		void loadKeyFrames(const tinygltf::Animation& animation, int bone, keyFrameList& keyFrames, tinygltf::AnimationChannel* translation, tinygltf::AnimationChannel* rotation, tinygltf::AnimationChannel* scale, tinygltf::AnimationChannel* weights);
+    ///Load all keyframes for the animation
+    void loadKeyFrames(const tinygltf::Animation& animation, int bone, keyFrameList& keyFrames, tinygltf::AnimationChannel* translation, tinygltf::AnimationChannel* rotation, tinygltf::AnimationChannel* scale, tinygltf::AnimationChannel* weights);
 
-		///All all animation for the skeleton
-		void loadSkeletonAnimations(tinygltf::Skin skin, const std::string& skeletonName);
+    ///All all animation for the skeleton
+    void loadSkeletonAnimations(tinygltf::Skin skin, const std::string& skeletonName);
 
-	public:
-		///Construct the skeleton importer
-		/// \param input model where the skeleton data is loaded from
-		skeletonImporter(tinygltf::Model& input);
+public:
+    ///Construct the skeleton importer
+    /// \param input model where the skeleton data is loaded from
+    skeletonImporter(tinygltf::Model& input);
 
-		///Return the constructed skeleton pointer
-		Ogre::v1::SkeletonPtr getSkeleton(const std::string& adapterName);
-	};
+    ///Return the constructed skeleton pointer
+    Ogre::v1::SkeletonPtr getSkeleton(const std::string& adapterName);
+};
 }
