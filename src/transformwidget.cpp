f212b1ee63f563e4c04f71a3bfec59124acface8
Matt Chang <chchwy@gmail.com>
2019-02-20 10:45:57 +1100

    Apply RTS to the current selected node

--- src/transformwidget.cpp
+++ src/transformwidget.cpp
@@ -5,6 +5,19 @@ TransformWidget::TransformWidget(QWidget* parent) : QWidget(parent),
     ui(new Ui::TransformWidget)
 {
     ui->setupUi(this);
+
+    auto doubleChanged = qOverload<double>(&QDoubleSpinBox::valueChanged);
+    connect(ui->tx, doubleChanged, this, &TransformWidget::positionChanged);
+    connect(ui->ty, doubleChanged, this, &TransformWidget::positionChanged);
+    connect(ui->tz, doubleChanged, this, &TransformWidget::positionChanged);
+
+    connect(ui->rx, doubleChanged, this, &TransformWidget::rotationChanged);
+    connect(ui->ry, doubleChanged, this, &TransformWidget::rotationChanged);
+    connect(ui->rz, doubleChanged, this, &TransformWidget::rotationChanged);
+
+    connect(ui->sx, doubleChanged, this, &TransformWidget::scaleChanged);
+    connect(ui->sy, doubleChanged, this, &TransformWidget::scaleChanged);
+    connect(ui->sz, doubleChanged, this, &TransformWidget::scaleChanged);
 }
 
 TransformWidget::~TransformWidget()
@@ -17,6 +30,8 @@ void TransformWidget::sceneNodeSelected(Ogre::SceneNode* node)
     if (node == nullptr)
         return;
 
+    mCurrentNode = node;
+
     Ogre::Vector3 pos = node->getPosition();
 
     QSignalBlocker b1(ui->tx);
@@ -26,7 +41,7 @@ void TransformWidget::sceneNodeSelected(Ogre::SceneNode* node)
     ui->ty->setValue(pos.y);
     ui->tz->setValue(pos.z);
     
-    const Ogre::Quaternion quaternion = node->_getDerivedOrientation();
+    const Ogre::Quaternion quaternion = node->getOrientation();
     Ogre::Matrix3 matrix3;
     quaternion.ToRotationMatrix(matrix3);
 
@@ -48,4 +63,38 @@ void TransformWidget::sceneNodeSelected(Ogre::SceneNode* node)
     ui->sx->setValue(scale.x);
     ui->sy->setValue(scale.y);
     ui->sz->setValue(scale.z);
+}
+
+void TransformWidget::positionChanged()
+{
+    if (mCurrentNode)
+    {
+        mCurrentNode->setPosition(Ogre::Real(ui->tx->value()), 
+                                  Ogre::Real(ui->ty->value()),
+                                  Ogre::Real(ui->tz->value()));
+    }
+}
+
+void TransformWidget::rotationChanged()
+{
+    if (mCurrentNode)
+    {
+        Ogre::Matrix3 matrix3;
+        matrix3.FromEulerAnglesXYZ(Ogre::Degree(ui->rx->value()),
+                                   Ogre::Degree(ui->ry->value()),
+                                   Ogre::Degree(ui->rz->value()));
+        Ogre::Quaternion q;
+        q.FromRotationMatrix(matrix3);
+        mCurrentNode->setOrientation(q);
+    }
+}
+
+void TransformWidget::scaleChanged()
+{
+    if (mCurrentNode)
+    {
+        mCurrentNode->setScale(Ogre::Real(ui->sx->value()),
+                               Ogre::Real(ui->sy->value()),
+                               Ogre::Real(ui->sz->value()));
+    }
 }
