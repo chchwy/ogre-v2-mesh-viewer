5e10c73d5f6011b1e2d12955802c4585d943d4ba
Matt Chang <chchwy@gmail.com>
2019-02-28 13:41:20 +1100

    Update the window title

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -74,7 +74,7 @@ MainWindow::MainWindow()
     createDockWindows();
 
     // Set the title
-    setWindowTitle("Ogre v2 Mesh Viewer v" APP_VERSION_NUMBER);
+    setWindowTitle("Ogre v2.1 Mesh Viewer v" APP_VERSION_NUMBER);
     readSettings();
 
     connect(mOgreManager, &OgreManager::sceneCreated, this, &MainWindow::onSceneLoaded);
