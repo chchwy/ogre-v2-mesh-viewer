075e3a6285bdeb45b6be15f492c88ad4c1de630a
Matt Chang <chchwy@gmail.com>
2019-05-21 11:19:40 +1000

    Using OpenGL3+ as the default render system because of some bugs in D3D11 render system

--- deploy.bat
+++ deploy.bat
@@ -2,13 +2,15 @@ xcopy /y "%OGREHOME%\bin\Debug\OgreMain_d.dll"                "dbin"
 xcopy /y "%OGREHOME%\bin\Debug\OgreHlmsPbs_d.dll"             "dbin"
 xcopy /y "%OGREHOME%\bin\Debug\OgreHlmsUnlit_d.dll"           "dbin"
 xcopy /y "%OGREHOME%\bin\Debug\OgreOverlay_d.dll"             "dbin"
-xcopy /y "%OGREHOME%\bin\Debug\RenderSystem_Direct3D11_d.dll" "dbin"
+rem xcopy /y "%OGREHOME%\bin\Debug\RenderSystem_Direct3D11_d.dll" "dbin"
+xcopy /y "%OGREHOME%\bin\Debug\RenderSystem_GL3Plus_d.dll"    "dbin"
 
 xcopy /y "%OGREHOME%\bin\Release\OgreMain.dll"                "bin"
 xcopy /y "%OGREHOME%\bin\Release\OgreHlmsPbs.dll"             "bin"
 xcopy /y "%OGREHOME%\bin\Release\OgreHlmsUnlit.dll"           "bin"
 xcopy /y "%OGREHOME%\bin\Release\OgreOverlay.dll"             "bin"
-xcopy /y "%OGREHOME%\bin\Release\RenderSystem_Direct3D11.dll" "bin"
+rem xcopy /y "%OGREHOME%\bin\Release\RenderSystem_Direct3D11.dll" "bin"
+xcopy /y "%OGREHOME%\bin\Release\RenderSystem_GL3Plus.dll"    "bin"
 
 pushd .
 call "%QTDIR%\bin\qtenv2.bat"
