a9d69bc16e29be53fb79d18efc97e0c78d9472ad
Matt Chang <chchwy@gmail.com>
2019-05-31 14:12:22 +1000

    Version bump v0.4.0

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -75,7 +75,7 @@ MainWindow::MainWindow()
     createDockWindows();
 
     // Set the title
-    setWindowTitle("Ogre v2 Mesh Viewer [v0.3.3]");
+    setWindowTitle("Ogre v2 Mesh Viewer [v0.4.0]");
     readSettings();
 
     connect(mOgreManager, &OgreManager::sceneCreated, this, &MainWindow::onSceneLoaded);
